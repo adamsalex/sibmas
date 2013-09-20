abbc027e41dcd1fe2bdd40f42cbf93b36c86c0a8
Shornikov Alexey <adamsalex@yandex.ru>
2013-09-20 12:34:56 +0700

    sibmas new bd

---
+++ myapp/config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Myapp::Application.config.secret_key_base = 'ae36480266883a9d72063e81a7d2a3a52754614740a964dba4dcae2a756fc1f8d8e1d501e3055041afc8825d3f577c862b79b63891f02a542d6141e8c56b0b3c'
---
+++ sibmas/config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Sibmas::Application.config.secret_key_base = '5ee5d218787069ccf8eda3159a7f8d3795782de6913395ca96bf74128f0cea0ead19a0f96229b0fc23465a81c34cddccee62e9b463d212349e6868232df0f6c4'

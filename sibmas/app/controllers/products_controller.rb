9fbeac78d1e7327ac30ab400d7180a6c64f408db
Shornikov Alexey <adamsalex@yandex.ru>
2013-09-20 13:04:50 +0700

    Product test

--- sibmas/app/controllers/products_controller.rb
+++ sibmas/app/controllers/products_controller.rb
@@ -69,6 +69,6 @@ class ProductsController < ApplicationController
 
     # Never trust parameters from the scary internet, only allow the white list through.
     def product_params
-      params.require(:product).permit(:name_ru, :, :product_code, :weight, :price, :list_price, :slug, :description_ru, :meta_description_ru, :meta_title_ru)
+      params.require(:product).permit(:name_ru, :product_code, :weight, :price, :list_price, :slug, :description_ru, :meta_description_ru, :meta_title_ru)
     end
 end
--- sibmas/app/views/products/index.json.jbuilder
+++ sibmas/app/views/products/index.json.jbuilder
@@ -1,4 +1,4 @@
 json.array!(@products) do |product|
-  json.extract! product, :name_ru, :, :product_code, :weight, :price, :list_price, :slug, :description_ru, :meta_description_ru, :meta_title_ru
+  json.extract! product, :name_ru, :product_code, :weight, :price, :list_price, :slug, :description_ru, :meta_description_ru, :meta_title_ru
   json.url product_url(product, format: :json)
 end
